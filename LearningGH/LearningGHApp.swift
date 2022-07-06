b867ec2c5daa4ade2ab0e6b4760a3369c52ee287
Osver Fernandez <osverdev@icloud.com>
2022-07-06 11:17:44 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Osver Fernandez on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
